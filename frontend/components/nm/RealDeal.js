c57628cbb408c91e32f532dac46447af98d0c139
“Minutzu321” <chirusmina@gmail.com>
2022-04-24 21:53:42 +0100

    trebuie

--- frontend/components/nm/RealDeal.js
+++ frontend/components/nm/RealDeal.js
@@ -122,7 +122,7 @@ export default function RealDeal({user, fetchUser, snack}) {
                     setIdat(udat);
                     if(!udat.next && !udat.terminat){
                       setAlege(false);
-                      
+                      //TODO trebuie sa alegem!
                       setIndiciu(udat.indiciu);
                       setStats(udat.total-udat.ramase+"/"+udat.total);
                       setArataPoza(udat.arataPoza);
