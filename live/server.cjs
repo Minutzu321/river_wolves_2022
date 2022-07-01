e20bb9287075a52e12c56b6861c0285013830133
“Minutzu321” <chirusmina@gmail.com>
2022-07-01 12:09:41 +0100

    live

--- live/server.cjs
+++ live/server.cjs
@@ -1,15 +1,21 @@
 const ws = require('ws')
+const Prisma = require('prisma/prisma-client');
 
 const server = new ws.Server({
   port: 8192
-})
-
+});
 console.log("Server pornit..")
+
+const prisma = new Prisma.PrismaClient()
+console.log("Conectat la baza de date..")
+
+
 
 const sockets = []
 
-server.on('connection', function (socket) {
+server.on('connection', function (socket, req) {
   sockets.push(socket)
+  let path = req.url;
   socket.on('message', onMessage)
   socket.on('close', function () {
     sockets.splice(sockets.indexOf(socket), 1)
