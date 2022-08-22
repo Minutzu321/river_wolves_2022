f25c2735c4be37c3dbf952a7200bdeca427cf4c0
Minutzu321 <chirusmina@gmail.com>
2022-08-22 19:07:13 +0300

    update

---
+++ frontend/components/Header/HeaderLinks.js
@@ -0,0 +1,132 @@
+/*eslint-disable*/
+import React from "react";
+import Link from "next/link";
+
+// @mui/core components
+import { makeStyles } from "@mui/styles";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Tooltip from "@mui/material/Tooltip";
+import Icon from "@mui/material/Icon";
+
+// @mui/icons
+import AppsIcon from '@mui/icons-material/Apps';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from "@mui/material/IconButton";
+
+// core components
+import CustomDropdown from "/components/CustomDropdown/CustomDropdown.js";
+import Button from "/components/CustomButtons/Button.js";
+
+import styles from "/styles/jss/nextjs-material-kit/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          navDropdown
+          buttonText="Components"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={AppsIcon}
+          dropdownList={[
+            <Link href="/components">
+              <a className={classes.dropdownLink}>All components</a>
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/nextjs-material-kit/#/documentation?ref=njsmk-navbar"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmk-navbar"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <Icon className={classes.icons}>unarchive</Icon> Upgrade to PRO
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/nextjs-material-kit?ref=njsmk-navbar"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <CloudDownloadIcon className={classes.icons} /> Download
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        {/*<Tooltip title="Delete">
+          <IconButton aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>*/}
+        <Tooltip
+          id="instagram-twitter"
+          title="Follow us on twitter"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://twitter.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-twitter"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-facebook"
+          title="Follow us on facebook"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.facebook.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-facebook"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
---
+++ frontend/pages-sections/LandingPage-Sections/ProductSection.js
@@ -0,0 +1,67 @@
+import React from "react";
+// @mui/core components
+import { makeStyles } from "@mui/styles";
+
+// @mui/icons
+import ChatIcon from '@mui/icons-material/Chat';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
+// core components
+import GridContainer from "/components/Grid/GridContainer.js";
+import GridItem from "/components/Grid/GridItem.js";
+import InfoArea from "/components/InfoArea/InfoArea.js";
+
+import styles from "/styles/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProductSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Let{"'"}s talk product</h2>
+          <h5 className={classes.description}>
+            This is the paragraph where you can write more details about your
+            product. Keep you user engaged by providing meaningful information.
+            Remember that by this time, the user is curious, otherwise he wouldn
+            {"'"}t scroll to get here. Add a button if you want the user to see
+            more.
+          </h5>
+        </GridItem>
+      </GridContainer>
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Free Chat"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={ChatIcon}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Verified Users"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={VerifiedUserIcon}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Fingerprint"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={FingerprintIcon}
+              iconColor="danger"
+              vertical
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
